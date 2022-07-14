54b75c077f6d801f2ec8268327b240bb2b4a7dda
Celso Octaviano De Carvalho <celso@celso>
2022-07-14 21:46:47 +0200

    resolved conflitct

---
+++ src/main/java/com/octodb/tutorialapi/TutorialApiApplication.java
@@ -0,0 +1,13 @@
+package com.octodb.tutorialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/octodb/tutorialapi/TutorialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octodb.tutorialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
